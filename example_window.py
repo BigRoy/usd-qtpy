fcb397c42b2e8d106bdbb78b04146631dafb3b95
Sasbom <saskervg@live.nl>
2023-11-22 22:51:07 +0100

    Allowed the example window to be ran with a simple path argument, and properly set up a main function body.

--- example_window.py
+++ example_window.py
@@ -1,5 +1,6 @@
 from qtpy import QtWidgets
 from pxr import Usd
+import sys
 
 from usd_qtpy import prim_hierarchy, layer_editor, prim_spec_editor
 
@@ -38,12 +39,23 @@ class Window(QtWidgets.QDialog):
         prim_spec_editor_widget = prim_spec_editor.SpecEditorWindow(stage=stage)
         splitter.addWidget(prim_spec_editor_widget)
 
-
-if __name__ == "__main__":
+def launch_window(argv : list[str]) -> int:
     path = "/path/to/usd/file.usda"
+    
+    # retrieve 1st argument as path. (index 0 is always __file__)
+    # More advanced parsing of arguments might be wanted in future.
+    if argv:
+        path = argv[1]
+
     stage = Usd.Stage.Open(path)
     app = QtWidgets.QApplication()
     dialog = Window(stage=stage)
     dialog.show()
     dialog.resize(600, 600)
     app.exec_()
+    return 0
+
+
+if __name__ == "__main__":
+    # POSIX compliant entrypoint
+    sys.exit(launch_window(sys.argv))
