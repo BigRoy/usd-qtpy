fbb7ab7d586cc10421fcb19ac24d554b1ba7f1b6
Sasbom <saskervg@live.nl>
2023-12-07 01:04:08 +0100

    Changed template file to .usda, fixed up minor things in turntable.

--- usd_qtpy/render_util/turntable.py
+++ usd_qtpy/render_util/turntable.py
@@ -68,7 +68,7 @@ def create_turntable_camera(stage: Usd.Stage,
     xform_path = xform.GetPath()
 
     cam = framing_camera.create_framing_camera_in_stage(
-        stage, xform_path, name, fit, width, height, True)
+        stage, xform_path, name, fit, width, height)
 
     return cam
 
@@ -98,7 +98,7 @@ def add_turntable_spin_op(xformable: UsdGeom.Xformable,
 
 
 def turntable_from_file(stage: Usd.Stage,
-                        turntable_filename: str = R"./assets/turntable/turntable_preset.usd",
+                        turntable_filename: str = R"./assets/turntable/turntable_preset.usda",
                         export_path: str = R"./temp/render",
                         renderer: str = "GL"):
     """
@@ -212,16 +212,16 @@ def turntable_from_file(stage: Usd.Stage,
     # center geometry.
     if subject_zup:
         subject_center_translate = Gf.Vec3d(
-                                            -subject_centroid[0] / min_sizediff,
-                                            subject_centroid[2] / min_sizediff,
-                                            -subject_bounds_min[1] / min_sizediff
-                                            )
+                                            -subject_centroid[0],
+                                            subject_centroid[2],
+                                            -subject_bounds_min[1]
+                                            ) / min_sizediff
     else:
         subject_center_translate = Gf.Vec3d(
-                                            -subject_centroid[0] / min_sizediff,
-                                            -subject_bounds_min[1] / min_sizediff,
-                                            -subject_centroid[2] / min_sizediff
-                                            )
+                                            -subject_centroid[0] ,
+                                            -subject_bounds_min[1],
+                                            -subject_centroid[2]
+                                            ) / min_sizediff
     
     subject_ref_xformable.AddTranslateOp(UsdGeom.XformOp.PrecisionDouble,"center_centroid")\
                          .Set(subject_center_translate)
