cb2d3afa2617f02bae86f6312a35f64a5f3aa411
Sasbom <saskervg@live.nl>
2023-12-08 15:26:10 +0100

    fleshing out dialog for playblasting

--- usd_qtpy/render_util/dialog.py
+++ usd_qtpy/render_util/dialog.py
@@ -3,12 +3,14 @@
 import os
 import re
 from typing import Any
+from functools import partial
 
 from qtpy import QtWidgets, QtCore
 from pxr import Usd
 from pxr.Usdviewq.stageView import StageView
 
 from . import playblast
+from ..resources import get_icon
 
 
 def _rectify_path_framenumberspec(path: str, padding: int =  4):
@@ -91,31 +93,79 @@ class PlayblastDialog(QtWidgets.QDialog):
     def __init__(self, parent: QtCore.QObject) -> Any:
         super(PlayblastDialog,self).__init__(parent=parent)
         self.setWindowTitle("USD Playblast")
-        
-        #self._layout.setContentsMargins(10,10,10,10)
 
-        width, height = 600, 800
-        geometry = QtCore.QRect(0, 0, width, height)
+        self.setStyleSheet("QToolButton::menu-indicator {"
+                           "width: 0px;"
+                           "}")
+
+        width, height, margin = 600, 800, 15
+        geometry = QtCore.QRect(margin, margin, 
+                                width - margin * 2, height - margin * 2)
 
         self.resize(QtCore.QSize(width,height))
 
-        self.vlayout = QtWidgets.QVBoxLayout()
-        self.vlayout.addSpacing(30)
-
         self._container = QtWidgets.QGroupBox(self)
-        self._container.setTitle("Playblast settings")
+        self._container.setTitle("Playblast settings...")
         self._container.setGeometry(geometry)
+
+        self.vlayout = QtWidgets.QVBoxLayout()
+        self.vlayout.addSpacing(30) # add some spacing from the top
         self._container.setLayout(self.vlayout)
 
-        btn_playblast = QtWidgets.QPushButton()
-        btn_playblast.setText("Playblast!")
+        self.btn_playblast = QtWidgets.QPushButton()
+        self.btn_playblast.setText("Playblast!")
 
         self.hlayout = QtWidgets.QFormLayout()
         self.hlayout.setHorizontalSpacing(200)
 
-        test_combobox = QtWidgets.QComboBox()
-        self.hlayout.addRow("Test",test_combobox)
+        # Resolution
+        self.spinbox_horresolution = QtWidgets.QSpinBox()
+        self.spinbox_verresolution = QtWidgets.QSpinBox()
+        self.spinbox_horresolution.setMaximum(4096)
+        self.spinbox_horresolution.setMinimum(1)
+        self.spinbox_verresolution.setMaximum(4096)
+        self.spinbox_verresolution.setMinimum(1)
+        self.spinbox_horresolution.setValue(1920)
+        self.spinbox_verresolution.setValue(1080)
         
-        self.vlayout.addLayout(self.hlayout)
+        self.btn_resolution = QtWidgets.QToolButton(icon=get_icon("chevron-down"))
+        self.btn_resolution.clicked.connect(self.btn_resolution.showMenu)
+        self.btn_resolution.setFixedSize(QtCore.QSize(30, 30))
 
-        self.vlayout.addWidget(btn_playblast)
+        self.btn_resolution_menu = QtWidgets.QMenu(self)
+        resolution_presets = {"SD 480p"   : (640, 480),
+                              "HD 720p"   : (1280, 720),
+                              "HD 1080p"  : (1920, 1080),
+                              "UHD 4K"    : (3840, 2160),
+                              "1k Square" : (1024, 1024),
+                              "2k Square" : (2048, 2048),
+                              "4k Square" : (4096, 4096)
+                              }
+
+        for label, res_tuple in resolution_presets.items():
+            action = self.btn_resolution_menu.addAction(label)
+            action.triggered.connect(partial(self._update_resolution, res_tuple))
+            action.setData(res_tuple)
+
+        self.btn_resolution.setMenu(self.btn_resolution_menu)
+
+        resolution_horlayout = QtWidgets.QHBoxLayout()
+        resolution_horlayout.addWidget(self.spinbox_horresolution)
+        resolution_horlayout.addWidget(self.spinbox_verresolution)
+        resolution_horlayout.addWidget(self.btn_resolution)
+        self.hlayout.addRow("Resolution", resolution_horlayout)
+
+
+        # Renderer Combobox
+        cbox_renderer = QtWidgets.QComboBox()
+        cbox_renderer.addItems(playblast.iter_renderplugin_names())
+        cbox_renderer.setCurrentIndex(0)
+        self.hlayout.addRow("Test",cbox_renderer)
+        
+
+        self.vlayout.addLayout(self.hlayout)
+        self.vlayout.addWidget(self.btn_playblast)
+
+    def _update_resolution(self, res_tuple: tuple[int,int]):
+        self.spinbox_horresolution.setValue(res_tuple[0])
+        self.spinbox_verresolution.setValue(res_tuple[1])
