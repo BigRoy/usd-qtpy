f8ffa7ada161d014a311c99f9e603f317aa06a2e
Roy Nieterau <roy_nieterau@hotmail.com>
2023-12-19 16:29:30 +0100

    Refactor imports

--- usd_qtpy/render_util/__init__.py
+++ usd_qtpy/render_util/__init__.py
@@ -1,46 +1,62 @@
-# module named render_util to not collide in names with houdini's render.py
-from .playblast import *
-from .framing_camera import *
-from .turntable import *
-from .dialog import *
-from .base import *
+from .base import (
+    RenderReportable,
+    using_tempfolder,
+    TempStageOpen,
+)
+from .dialog import (
+    prompt_input_path,
+    prompt_output_path,
+    PlayblastDialog,
+    TurntableDialog,
+)
+from .framing_camera import (
+    get_stage_up,
+    create_framing_camera_in_stage,
+    camera_conform_sensor_to_aspect,
+)
+from .playblast import (
+    camera_from_stageview,
+    iter_stage_cameras,
+    get_file_cameras,
+    get_frames_string,
+    tuples_to_frames_string,
+    render_playblast,
+)
+from .turntable import (
+    create_turntable_xform,
+    create_turntable_camera,
+    get_turntable_frames_string,
+    turntable_from_file,
+    get_file_timerange_as_string,
+    file_is_zup,
+)
 
 
-from_base = (RenderReportable, 
-             using_tempfolder,
-             TempStageOpen
-             )
+__all__ = [
+    "RenderReportable",
+    "using_tempfolder",
+    "TempStageOpen",
 
-from_dialog = (prompt_input_path, 
-               prompt_output_path, 
-               PlayblastDialog, 
-               TurntableDialog
-               )
+    "prompt_input_path",
+    "prompt_output_path",
+    "PlayblastDialog",
+    "TurntableDialog",
 
-from_turntable = (create_turntable_xform, 
-                  create_turntable_camera, 
-                  get_turntable_frames_string,
-                  turntable_from_file,
-                  get_file_timerange_as_string,
-                  file_is_zup
-                  )
+    "get_stage_up",
+    "create_framing_camera_in_stage",
+    "camera_conform_sensor_to_aspect",
 
-from_playblast = (camera_from_stageview,
-                  iter_stage_cameras,
-                  get_file_cameras,
-                  get_frames_string,
-                  tuples_to_frames_string,
-                  render_playblast
-                  )
+    "camera_from_stageview",
+    "iter_stage_cameras",
+    "get_file_cameras",
+    "get_frames_string",
+    "tuples_to_frames_string",
+    "render_playblast",
 
-from_framing_camera = (get_stage_up,
-                       create_framing_camera_in_stage,
-                       camera_conform_sensor_to_aspect,
-                       )
-
-# Unroll into __all__ and expose
-__all__ = [*from_base,
-           *from_dialog,
-           *from_turntable,
-           *from_playblast,
-           *from_framing_camera]
+    "create_turntable_xform",
+    "create_turntable_camera",
+    "get_turntable_frames_string",
+    "turntable_from_file",
+    "get_file_timerange_as_string",
+    "file_is_zup",
+]
